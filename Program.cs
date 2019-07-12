eaf1dd6c4371455a9bc516424c15878765474ebc
Marcin Malessa <marcin@malessa.pl>
2019-07-12 23:13:38 +0200

    Move config to yaml file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Rest;
 using System;
 using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace testPowerBi
 {
@@ -9,31 +11,31 @@ namespace testPowerBi
     {
         static void Main(string[] args)
         {
-            // try {
-                var tokenFileName = "token.txt";
-                var tsr = new StreamReader(tokenFileName);
-                var tokenString = tsr.ReadToEnd();
-            
-                var credentials = new TokenCredentials(tokenString);
-                var client = new PowerBIClient(credentials);
+            var configFilename = "config.yaml";
+            var input = new StreamReader(configFilename);
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            var config = deserializer.Deserialize<Cfg>(input);
+            var credentials = new TokenCredentials(config.token);
+            var client = new PowerBIClient(credentials);
+            var reportsResult = client.Reports.GetReportsInGroup(config.workspaceid);
 
-                var workspaceFileName = "workspace.txt";
-                var wsr = new StreamReader(workspaceFileName);
-                var workspaceId = wsr.ReadToEnd();
-                var reportsResult = client.Reports.GetReportsInGroup(workspaceId);
+            foreach (var report in reportsResult.Value)
+            {
+                Console.WriteLine("Name:      " + report.Name);
+                Console.WriteLine("Id:        " + report.Id);
+                Console.WriteLine("DatasetId: " + report.DatasetId);
+                Console.WriteLine("EmbedUrl:  " + report.EmbedUrl);
+                Console.WriteLine("WebUrl:    " + report.WebUrl);
+                Console.WriteLine("");
+            }
+        }
 
-                foreach (var report in reportsResult.Value)
-                {
-                    Console.WriteLine("Name:      " + report.Name);
-                    Console.WriteLine("Id:        " + report.Id);
-                    Console.WriteLine("DatasetId: " + report.DatasetId);
-                    Console.WriteLine("EmbedUrl:  " + report.EmbedUrl);
-                    Console.WriteLine("WebUrl:    " + report.WebUrl);
-                    Console.WriteLine("");
-                }
-            // } catch {
-            //     Console.WriteLine("Something went wrong...");
-            // }
+        public class Cfg
+        {
+            public string token { get; set; }
+            public string workspaceid {get; set; }
         }
     }
 }
